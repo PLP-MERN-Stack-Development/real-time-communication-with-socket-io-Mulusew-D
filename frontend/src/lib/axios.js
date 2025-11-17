3d3214229e9edc768c6be79b1848bedf53109c7f
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-17 18:27:09 +0300

    initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-// Always use environment variable
-const BASE_URL = import.meta.env.VITE_API_URL;
+// Use VITE_API_URL if defined, otherwise fallback to localhost for development
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
